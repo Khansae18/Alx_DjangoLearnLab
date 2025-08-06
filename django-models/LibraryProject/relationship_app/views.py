8770ecef0d3c9bb7a968b7f81e265c968decd779
khansae <khansaeugc18@gmail.com>
2025-08-06 21:59:43 +0200

    tous

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,23 +1,9 @@
-from django.shortcuts import render
-from django.views.generic.detail import DetailView
-from .models import Book  
-from .models import Library  
-
-
-def list_books(request):
-    books = Book.objects.all()
-    return render(request, 'relationship_app/list_books.html', {'books': books})
-
-
-
-class LibraryDetailView(DetailView):
-    model = Library
-    template_name = 'relationship_app/library_detail.html'
-    context_object_name = 'library'
-    from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect
 from django.contrib.auth import login, logout, authenticate
 from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from django.contrib.auth.decorators import login_required
+from django.views.generic import DetailView
+from .models import Library, Book, Author
 
 # Register View
 def register_view(request):
@@ -26,7 +12,7 @@ def register_view(request):
         if form.is_valid():
             user = form.save()
             login(request, user)
-            return redirect('home')  # change 'home' to any view you want after login
+            return redirect('home')  # Redirect after successful registration
     else:
         form = UserCreationForm()
     return render(request, 'relationship_app/register.html', {'form': form})
@@ -38,7 +24,7 @@ def login_view(request):
         if form.is_valid():
             user = form.get_user()
             login(request, user)
-            return redirect('home')  # change 'home' to any view you want after login
+            return redirect('home')  # Redirect after login
     else:
         form = AuthenticationForm()
     return render(request, 'relationship_app/login.html', {'form': form})
@@ -48,4 +34,7 @@ def logout_view(request):
     logout(request)
     return render(request, 'relationship_app/logout.html')
 
-
+# Home view (optional but recommended for redirect)
+@login_required
+def home_view(request):
+    return render(request, 'relationship_app/home.html')
