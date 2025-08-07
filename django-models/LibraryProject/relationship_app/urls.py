aa9313d39d7776f6038d5e0b2bc524e007de851e
khansae <khansaeugc18@gmail.com>
2025-08-07 21:20:11 +0200

    new

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('add-book/', views.add_book, name='add_book'),
-    path('edit-book/<int:book_id>/', views.edit_book, name='edit_book'),
-    path('delete-book/<int:book_id>/', views.delete_book, name='delete_book'),
+    path('books/add/', views.add_book, name='add_book'),
+    path('books/<int:book_id>/edit/', views.edit_book, name='edit_book'),
+    path('books/<int:book_id>/delete/', views.delete_book, name='delete_book'),
 ]
 
 
