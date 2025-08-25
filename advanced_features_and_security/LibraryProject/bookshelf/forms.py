c16d88ccb568904421b24190a963d982d3537a49
khansae <khansaeugc18@gmail.com>
2025-08-25 14:32:25 +0200

    bel

--- advanced_features_and_security/LibraryProject/bookshelf/forms.py
+++ advanced_features_and_security/LibraryProject/bookshelf/forms.py
@@ -1,12 +1,8 @@
 from django import forms
+from .models import Book
 
-class BookForm(forms.Form):
-    title = forms.CharField(max_length=200, strip=True)
-    author = forms.CharField(max_length=200, strip=True)
-    published_date = forms.DateField(
-        required=False,
-        input_formats=["%Y-%m-%d"],  # YYYY-MM-DD
-    )
+class ExampleForm(forms.ModelForm):
+    class Meta:
+        model = Book
+        fields = ['title', 'author', 'published_date']
 
-class SearchForm(forms.Form):
-    q = forms.CharField(max_length=100, required=False, strip=True)
--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,41 +1,13 @@
 from django.shortcuts import render, redirect
-from django.contrib import messages
-from django.core.paginator import Paginator
-from django.views.decorators.http import require_http_methods
-from django.contrib.auth.decorators import login_required, permission_required
+from .forms import ExampleForm
 
-from .models import Book
-from .forms import BookForm, SearchForm
-
-@require_http_methods(["GET"])
-def book_list(request):
-    form = SearchForm(request.GET or None)
-    qs = Book.objects.all().order_by("-id")
-
-    if form.is_valid():
-        q = form.cleaned_data.get("q") or ""
-        if q:
-            # Safe: ORM builds a parameterized query (no SQL injection).
-            qs = qs.filter(title__icontains=q) | qs.filter(author__icontains=q)
-
-    paginator = Paginator(qs, 10)
-    page_obj = paginator.get_page(request.GET.get("page"))
-    return render(request, "bookshelf/book_list.html", {"page_obj": page_obj, "form": form})
-
-@login_required
-@permission_required("bookshelf.add_book", raise_exception=True)
-@require_http_methods(["GET", "POST"])
-def create_book(request):
-    if request.method == "POST":
-        form = BookForm(request.POST)
+def example_form_view(request):
+    if request.method == 'POST':
+        form = ExampleForm(request.POST)
         if form.is_valid():
-            Book.objects.create(
-                title=form.cleaned_data["title"],
-                author=form.cleaned_data["author"],
-                published_date=form.cleaned_data.get("published_date"),
-            )
-            messages.success(request, "Book created.")
-            return redirect("bookshelf:book_list")
+            form.save()
+            return redirect('book_list')  # change to your list view
     else:
-        form = BookForm()
-    return render(request, "bookshelf/form_example.html", {"form": form})
+        form = ExampleForm()
+    return render(request, 'bookshelf/form_example.html', {'form': form})
+
