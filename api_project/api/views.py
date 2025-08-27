07fffd6302dc5de02f2072cc13320142f3f585ff
khansae <khansaeugc18@gmail.com>
2025-08-27 23:42:47 +0200

    api

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,11 +1,6 @@
-from rest_framework.response import Response
-from rest_framework.decorators import api_view
-from .models import Book
-from .serializers import BookSerializer
+from rest_framework.views import APIView
 
-@api_view(['GET'])
-def book_list(request):
-    books = Book.objects.all()
-    serializer = BookSerializer(books, many=True)
-    return Response(serializer.data)
+class BookList(APIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
 
--- api_project/api_project/settings.py
+++ api_project/api_project/settings.py
@@ -23,7 +23,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-gvu(#e$)ezi=j7&cm4^xec0y8)xxw=kx_1^q27az9ao!m%%w6)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
+
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
