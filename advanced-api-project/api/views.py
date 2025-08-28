9a993c17098df1b276fa9f73ba80e5f1ca51791a
khansae <khansaeugc18@gmail.com>
2025-08-28 14:50:47 +0200

    Api

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,7 +1,7 @@
 from rest_framework import generics, permissions
 from django_filters import rest_framework 
 from django_filters.rest_framework import DjangoFilterBackend 
-from rest_framework.filters import SearchFilter, OrderingFilter 
+from rest_framework.filters import OrderingFilter, SearchFilter
 from .models import Book
 from .serializers import BookSerializer
 from rest_framework import filters
