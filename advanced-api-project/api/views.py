4d27b3654f7d96ec9240d7d9407c6a6c3ae3d290
khansae <khansaeugc18@gmail.com>
2025-08-28 14:17:38 +0200

    api

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -8,8 +8,9 @@ class BookListView(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     permission_classes = [permissions.AllowAny]  # Anyone can view
-     filter_backends = [filters.SearchFilter]
+    filter_backends = [filters.SearchFilter]
     search_fields = ['title', 'author__name']
+
 
 # Retrieve a single book
 class BookDetailView(generics.RetrieveAPIView):
@@ -22,24 +23,14 @@ class BookCreateView(generics.CreateAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     permission_classes = [permissions.IsAuthenticated]  # Only logged-in users
-    def perform_create(self, serializer):
-        """
-        Customize saving behavior.
-        For example, automatically associate the logged-in user as the creator.
-        """
-        serializer.save(created_by=self.request.user)  
+    
 
 # Update an existing book
 class BookUpdateView(generics.UpdateAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     permission_classes = [permissions.IsAuthenticated]
-    def perform_update(self, serializer):
-        """
-        Customize update behavior.
-        Example: log the user who updated the book.
-        """
-        serializer.save(updated_by=self.request.user)  
+    
 
 # Delete a book
 class BookDeleteView(generics.DestroyAPIView):
