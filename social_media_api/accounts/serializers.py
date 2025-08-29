0a1fae811b6e5092b1c709388ac963952b4c39f8
khansae <khansaeugc18@gmail.com>
2025-08-29 15:06:52 +0200

    social

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
 from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework.authtoken.models import Token 
 
 User = get_user_model()
 
@@ -17,11 +18,12 @@ class RegisterSerializer(serializers.ModelSerializer):
         fields = ['username', 'email', 'password']
 
     def create(self, validated_data):
-        user = User.objects.create_user(
+        user = get_user_model().objects.create_user(
             username=validated_data['username'],
             email=validated_data['email'],
             password=validated_data['password']
-        )
+            Token.objects.create(user=user)
+          )
         return user
 
 class LoginSerializer(serializers.Serializer):
--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -1,24 +1,24 @@
-from rest_framework.response import Response
+from django.contrib.auth import get_user_model, authenticate
 from rest_framework.views import APIView
+from rest_framework.response import Response
 from rest_framework import status, permissions
-from django.contrib.auth import get_user_model
-from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework.authtoken.models import Token
 
 from .serializers import RegisterSerializer, LoginSerializer, UserSerializer
 
 User = get_user_model()
 
+
 class RegisterView(APIView):
     def post(self, request):
         serializer = RegisterSerializer(data=request.data)
         if serializer.is_valid():
-            user = serializer.save()
-            refresh = RefreshToken.for_user(user)
+            user = serializer.save()  # creates user + token in serializer
+            token, created = Token.objects.get_or_create(user=user)
             return Response({
                 "user": UserSerializer(user).data,
-                "refresh": str(refresh),
-                "access": str(refresh.access_token)
-            })
+                "token": token.key
+            }, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
@@ -27,12 +27,11 @@ class LoginView(APIView):
         serializer = LoginSerializer(data=request.data)
         if serializer.is_valid():
             user = serializer.validated_data
-            refresh = RefreshToken.for_user(user)
+            token, created = Token.objects.get_or_create(user=user)
             return Response({
                 "user": UserSerializer(user).data,
-                "refresh": str(refresh),
-                "access": str(refresh.access_token)
-            })
+                "token": token.key
+            }, status=status.HTTP_200_OK)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
@@ -42,4 +41,5 @@ class ProfileView(APIView):
     def get(self, request):
         serializer = UserSerializer(request.user)
         return Response(serializer.data)
+
 
